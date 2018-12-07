2abf2304f18778a115d04fb0468ce17e71c3666e
Bigshrimp666 <pannijin@gmail.com>
2018-12-06 23:52:28 -0700

    Configure an endpoint
    Configure an endpoint for GetCourseDetailsRequest

--- src/main/java/com/soap/webservices/soapcoursemanagment/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/soap/webservices/soapcoursemanagment/soap/CourseDetailsEndpoint.java
@@ -1,11 +1,28 @@
 package com.soap.webservices.soapcoursemanagment.soap;
 
+import com.sample.courses.CourseDetails;
 import com.sample.courses.GetCourseDetailsRequest;
 import com.sample.courses.GetCourseDetailsResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+@Endpoint
 public class CourseDetailsEndpoint {
-    public GetCourseDetailsResponse processCourseDetailsRequest(GetCourseDetailsRequest request){
+
+    @PayloadRoot(namespace = "http://sample.com/courses", localPart = "GetCourseDetailsRequest")
+
+    @ResponsePayload
+    public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request){
         GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setId(request.getId());
+        courseDetails.setName("Microservices course");
+        courseDetails.setDescription("a good course");
+        response.setCourseDetails(courseDetails);
+
         return response;
     }
 }
