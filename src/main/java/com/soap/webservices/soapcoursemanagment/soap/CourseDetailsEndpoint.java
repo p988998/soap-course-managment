18ee2b3ede17799604f05efbf11f2f1844d36123
Bigshrimp666 <pannijin@gmail.com>
2018-12-06 17:42:06 -0700

    add xsd

---
+++ src/main/java/com/soap/webservices/soapcoursemanagment/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,11 @@
+package com.soap.webservices.soapcoursemanagment.soap;
+
+import com.sample.courses.GetCourseDetailsRequest;
+import com.sample.courses.GetCourseDetailsResponse;
+
+public class CourseDetailsEndpoint {
+    public GetCourseDetailsResponse processCourseDetailsRequest(GetCourseDetailsRequest request){
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        return response;
+    }
+}
